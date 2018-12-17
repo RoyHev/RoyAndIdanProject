7ba5706df193610da3a9e74a06498e99dc12dfee
idantp <idantp@gmail.com>
2018-12-17 23:41:06 +0200

    add data

--- Parser.cpp
+++ Parser.cpp
@@ -3,11 +3,17 @@
 //
 
 #include "Parser.h"
+#include <vector>
 
 Parser::Parser(Data mapsData) {
     this->mapsData = mapsData;
 }
 
 void Parser::parseFunc(vector<string> strings) {
-
+    Command command;
+    for(string str:strings){
+        if(this->mapsData.doesCommandExist(str)){
+            command = this->mapsData.getCommand(str);
+        }
+    }
 }
