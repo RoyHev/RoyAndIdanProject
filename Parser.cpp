4ecd0500f03c4f758e625f1f26e93574f5f1463d
idantp <idantp@gmail.com>
2018-12-19 19:06:32 +0200

    Merge branch 'master' of /home/idantp/CLionProjects/RoyAndIdanProject with conflicts.

--- Parser.cpp
+++ Parser.cpp
@@ -1,19 +1,37 @@
-////
-//// Created by roy on 17/12/18.
-////
 //
-//#include "Parser.h"
-//#include <vector>
+// Created by roy on 17/12/18.
 //
-//Parser::Parser(Data mapsData) {
-//    this->mapsData = mapsData;
-//}
-//
-//void Parser::parseFunc(vector<string> strings) {
-//    Command command;
-//    for(string str:strings){
-//        if(this->mapsData.doesCommandExist(str)){
-//            command = this->mapsData.getCommand(str);
-//        }
-//    }
-//}
+
+#include "Parser.h"
+#include <vector>
+
+#define OPEN_SERVER "openDataServer"
+#define IF_CONDITION "if"
+#define WHILE_LOOP "while"
+#define CONNECT "connect"
+#define VAR "var"
+#define PRINT "print"
+#define SLEEP "sleep"
+#define ASSIGN "="
+
+using namespace std;
+
+Parser::Parser(vector<string> lexStrings) {
+    this->lexStrings = lexStrings;
+    this->index = 0;
+
+}
+
+map<string, CommandExpression *> Parser::commandsGenerator() {
+    map<string, CommandExpression *> commandsExMap;
+    commandsExMap.emplace(OPEN_SERVER,new OpenDataServerCommand());
+    commandsExMap.insert(make_pair(OPEN_SERVER,new OpenDataServerCommand()));
+    commandsExMap.insert(make_pair(IF_CONDITION,IfCommand));
+    commandsExMap.insert(make_pair(WHILE_LOOP,LoopCommand));
+    commandsExMap.insert(make_pair(CONNECT,ConnectCommand));
+    commandsExMap.insert(make_pair(VAR,CreateVarCommand));
+    commandsExMap.insert(make_pair(PRINT,PrintCommand));
+    commandsExMap.insert(make_pair(SLEEP,SleepCommand));
+    commandsExMap.insert(make_pair(ASSIGN,AssignCommand));
+    return commandsExMap;
+}
--- Parser.h
+++ Parser.h
@@ -1,21 +1,30 @@
-////
-//// Created by roy on 17/12/18.
-////
 //
-//#ifndef EX3_PARSER_H
-//#define EX3_PARSER_H
+// Created by roy on 17/12/18.
 //
-//#include <string>
-//#include <vector>
-//#include "Data.h"
-//using namespace std;
-//
-//class Parser {
-//Data mapsData;
-//public:
-//    Parser(Data mapsData);
-//    void parseFunc(vector<string> strings);
-//};
-//
-//
-//#endif //EX3_PARSER_H
+
+#ifndef EX3_PARSER_H
+#define EX3_PARSER_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include "Command.h"
+#include "CommandExpression.h"
+#include "Data.h"
+
+using namespace std;
+
+class Parser {
+    vector<string> lexStrings;
+    map<string, CommandExpression *> commandsMap;
+    int index;
+public:
+    Parser(vector<string> lexStrings);
+
+private:
+    map<string,CommandExpression*> commandsGenerator();
+};
+
+
+#endif //EX3_PARSER_H
