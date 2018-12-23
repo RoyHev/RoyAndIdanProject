c9e51521c76abab4e719c8e75c694876c4b1f7f6
idantp <idantp@gmail.com>
2018-12-23 13:04:47 +0200

    change lexer

--- ConditionParser.cpp
+++ ConditionParser.cpp
@@ -22,6 +22,11 @@ ConditionParser::ConditionParser(VarManager *varManager) {
     this->varManager = varManager;
 }
 
+/*
+ * the function gets a vector of data which contains at least one condition or loop
+ * and an index that points to the first condition string. the function returns the index to the
+ * condition's corresponding BRACKET_CLOSER ("}").
+ */
 int ConditionParser::indexIncrement(int i, vector<string> data) {
     while (data.at(i) != BRACKET_OPENER) {
         i++;
@@ -33,7 +38,7 @@ int ConditionParser::indexIncrement(int i, vector<string> data) {
             bracketsRatio++;
         } else if (data.at(i) == BRACKET_CLOSER) {
             bracketsRatio--;
-            //don't push the last brackets
+            //don't skip the index of BRACKET_CLOSER
             if (bracketsRatio == 0) {
                 continue;
             }
@@ -43,7 +48,13 @@ int ConditionParser::indexIncrement(int i, vector<string> data) {
     return i;
 }
 
-//will execute one iteration
+/*
+ * the function gets a vector of data which contains at least one condition or loop
+ * and an index that points to the first condition string.
+ * the function creates a new vector<string> that describes the condition's scope only,
+ * and parses it.
+ * the function returns an index to the last element that describes the condition's scope.
+ */
 int ConditionParser::execute(int index, vector<string> data) {
     vector<string> scopeLexerVector;
     while (data.at(index) != BRACKET_OPENER) {
@@ -69,6 +80,13 @@ int ConditionParser::execute(int index, vector<string> data) {
     return index;
 }
 
+/*
+ * the function gets a vector of data which contains one condition or loop
+ * and an index that points to the first condition string.
+ * the function checks the condition (or the loop condition) and returns true if the
+ * condition is correct and false if the condition is not.
+ * if inside the condition there is not a condition operator, the function throws an error.
+ */
 bool ConditionParser::conditionCheck(int i, vector<string> data) {
     Parser *scopeParser = new Parser(data, varManager);
     ShuntingYard *sh = new ShuntingYard(varManager);
@@ -77,6 +95,7 @@ bool ConditionParser::conditionCheck(int i, vector<string> data) {
     string strOperand2 = data.at(i + 3);
     double operand2 = sh->evaluateInfix(strOperand2)->calculate();
     string operation = data.at(i + 2);
+    //find the condition operator and return true or false accordingly.
     if (operation == GREATER) {
         if (operand1 > operand2) {
             return true;
@@ -113,7 +132,8 @@ bool ConditionParser::conditionCheck(int i, vector<string> data) {
         } else {
             return false;
         }
-    } else {
+    }//for any string that does not descreibes a condition operator - throw an error.
+    else {
         throw runtime_error("Syntax Error - Invalid operator.");
     }
 
